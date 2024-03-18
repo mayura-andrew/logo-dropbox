7326191301195648fccc8f9596086026ae616f58
mayura-andrew <mayuraalahakoon@gmail.com>
2024-03-18 23:29:04 +0530

    added user details form

---
+++ src/components/dropbox.tsx
@@ -0,0 +1,72 @@
+import { SingleImageDropzone } from "@/components/single-image-dropzone";
+import { useEdgeStore } from "@/lib/edgestore";
+import Link from "next/link";
+import { useState } from "react";
+
+
+export default function Dropbox() {
+    const [file, setFile] = useState<File>();
+    const [progress, setProgress] = useState(0);
+    const [urls, setUrls] = useState<{
+      url: string;
+      thumbnailUrl: string | null;
+    }>();
+    const { edgestore } = useEdgeStore();
+  
+    const onSubmit = (data:any) => console.log(data);
+  
+    return (
+      <>
+    <div>
+      <SingleImageDropzone
+          width={200}
+          height={200}
+          value={file}
+          dropzoneOptions={{
+            maxSize: 1024 * 1024 * 5, // 1MB
+          }}
+          onChange={(file) => {
+            setFile(file);
+          } } />
+        <div className="h-[6px] w-44 border rounded overflow-hidden">
+          <div
+            className="h-full bg-white transition-all duration-150"
+            style={{
+              width: `${progress}%`,
+            }} />
+        </div>
+        <button
+          className="bg-white text-black rounded m-2 items-center hover:opacity-80 font-bold py-2 px-4  focus:outline-none focus:shadow-outline"
+          onClick={async () => {
+            if (file) {
+              const res = await edgestore.myPublicImages.upload({
+                file,
+                input: { type: "post" },
+                onProgressChange: (progress) => {
+                  setProgress(progress);
+                },
+              });
+              // save your data here
+              setUrls({
+                url: res.url,
+                thumbnailUrl: res.thumbnailUrl,
+              });
+            }
+          } }
+        >
+          Upload
+        </button>
+        {urls?.url && (
+          <Link href={urls.url} target="_blank">
+            URL
+          </Link>
+        )}
+        {urls?.thumbnailUrl && (
+          <Link href={urls.thumbnailUrl} target="_blank">
+            Review
+          </Link>
+        )}
+    </div>
+  </>
+    );
+  }
---
+++ src/components/userform.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const UserForm = () => {
+  return (
+    <div className="w-full max-w-xs ml-4">
+  <h2 className="text-lg font-bold text-white">Tell about you</h2>
+  <form className="bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div className="mb-4">
+      <label className="block text-white text-sm font-bold mb-2" htmlFor="username">
+        Name
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+    </div>
+    <div className="mb-4">
+      <label className="block text-white text-sm font-bold mb-2" htmlFor="registrationNumber">
+        Registration Number
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="registrationNumber" type="text" placeholder="Registration Number" />
+    </div>
+    <div className="mb-4">
+      <label className="block text-white text-sm font-bold mb-2" htmlFor="studyProgram">
+        Study Program
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="studyProgram" type="text" placeholder="Study Program" />
+    </div>
+    <div className="mb-4">
+      <label className="block text-white text-sm font-bold mb-2" htmlFor="faculty">
+        Faculty
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="faculty" type="text" placeholder="Faculty" />
+    </div>
+    <div className="mb-4">
+      <label className="block text-white text-sm font-bold mb-2" htmlFor="academicYear">
+        Academic Year
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="academicYear" type="text" placeholder="Academic Year" />
+    </div>
+    <div className="mb-6">
+      <label className="block text-white text-sm font-bold mb-2" htmlFor="email">
+        Email
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" />
+    </div>
+    <div className="flex items-center justify-between">
+      <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+        Submit
+      </button>
+    </div>
+  </form>
+</div>
+  );
+};
+
+export default UserForm;
