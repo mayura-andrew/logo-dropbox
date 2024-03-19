85d8a6ff71d1e1c29696674824c95bc05a25035c
mayura-andrew <mayuraalahakoon@gmail.com>
2024-03-19 10:21:24 +0530

    changed colors in background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Page() {
 
   return (
     <>
-      <div className="flex flex-col items-center gap-2 bg-black text-white">
+      <div className="flex flex-col items-center gap-2 bg-black text-white prose prose-lg max-w-none rounded-xl bg-gradient-to-r from-gray-800 via-black to-gray-">
         <div className="flex flex-col lg:flex-row m-8 gap-4">
           {showDetails && <CompetitionDetails />}
           <UserForm />
           <Dropbox />
           <button 
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded " 
+            className="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded " 
             onClick={() => setShowDetails(!showDetails)}
           >
             {showDetails ? 'Hide Details ⬆️ ' : 'About Competition ⬅️'}
--- src/components/description.tsx
+++ src/components/description.tsx
@@ -1,58 +1,57 @@
 import React from 'react';
+import BackImg from '../../public/bg1.avif';
 
 const CompetitionDetails = () => {
   return (
-    <div
-      style={{
-        backgroundImage: `url('https://4.bp.blogspot.com/-wWuTowwVP80/UQ-EOuk90WI/AAAAAAAAGxM/PtWJoPiL00I/s1600/wallpaper21.jpg')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
-    <div className="prose prose-lg max-w-non text-white rounded-lg">
-        <div className="prose prose-lg max-w-none">
-          <h1 className="text-3xl font-bold mb-4">Logo Design Competition</h1>
-          <h2 className='text-2xl font-mono font-bold mb-3'>Submit Your Creativity!</h2>
+<div className="prose prose-lg max-w-none text-white rounded-xl bg-gradient-to-r from-black via-pink-900 to-black m-1">
+        <div className="prose prose-lg max-w-none items-center">
+        <h1 className="text-3xl items-center font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-blue-400 via-yellow-500 to-white">
+          Logo Design Competition
+        </h1> 
+        <h1 className="text-2xl items-center font-bold mb-4 bg-clip-text text-white">
+        The Open University of Sri Lanka          
+        </h1>
+             
+        <h2 className='text-1xl font-mono font-bold mb-3'>Submit Your Creativity!</h2>
           <p>🎨 Welcome to the Logo Design Competition, where creativity meets innovation! We invite talented designers from all backgrounds to showcase their skills and contribute to the visual identity of our upcoming platform.</p>
 
           <h2 className="text-2xl font-bold mt-6 mb-2">How to Participate:</h2>
           <ol className="list-disc list-inside space-y-2">
-            <li className="bg-gray-800 p-2 rounded">Submit Your Design: Designers can submit their logo designs using our simple submission form. All entries must be original creations.</li>
-            <li className="bg-gray-500 p-2 rounded">Theme: The theme for the logo design is &quot;Innovation and Collaboration.&quot; Designers are encouraged to incorporate elements that represent our platform&apos;s core values.</li>
-            <li className="bg-gray-800 p-2 rounded">Format: Participants should submit their logos in vector format (AI, SVG, EPS) to ensure scalability. Along with this, a high-resolution PNG or JPEG preview of the logo should also be included. Please note that the participant&apos;s Registration Number must be included in the right-hand corner of the image file. For an example, please click here.</li>
-            <li className="bg-gray-500 p-2 rounded">Deadline: Submissions will be accepted until [deadline date]. Late entries will not be considered.</li>
+            <li className="bg-green-800 p-2 rounded">Submit Your Design: Designers can submit their logo designs using our simple submission form. All entries must be original creations.</li>
+            <li className="bg-gray-800 p-2 rounded">Theme: The theme for the logo design is &quot;Innovation and Collaboration.&quot; Designers are encouraged to incorporate elements that represent our platform&apos;s core values.</li>
+            <li className="bg-green-800 p-2 rounded">Format: Participants should submit their logos in vector format (AI, SVG, EPS) to ensure scalability. Along with this, a high-resolution PNG or JPEG preview of the logo should also be included. Please note that the participant&apos;s Registration Number must be included in the right-hand corner of the image file. For an example, please click here.</li>
+            <li className="bg-gray-800 p-2 rounded">Deadline: Submissions will be accepted until [deadline date]. Late entries will not be considered.</li>
           </ol>
 
           <h2 className="text-2xl font-bold mt-6 mb-2">Benefits of Participation:</h2>
           <ol className="list-disc list-inside space-y-2">
-            <li className="bg-gray-800 p-2 rounded">Recognition: Gain exposure and recognition for your design skills among our community and industry professionals.</li>
-            <li className="bg-gray-500 p-2 rounded">Prizes: Exciting prizes await the top three designs, including cash rewards, featured recognition on our platform, and more!</li>
-            <li className="bg-gray-800 p-2 rounded">Portfolio Enhancement: Showcase your creativity and talent by adding your logo design to your portfolio. It&apos;s a great opportunity to impress potential clients and employers.</li>
-            <li className="bg-gray-500 p-2 rounded">Contribution: Leave a lasting impact by contributing to the visual identity of our platform, shaping its brand image for years to come.</li>
+            <li className="bg-green-800 p-2 rounded">Recognition: Gain exposure and recognition for your design skills among our community and industry professionals.</li>
+            <li className="bg-gray-800 p-2 rounded">Prizes: Exciting prizes await the top three designs, including cash rewards, featured recognition on our platform, and more!</li>
+            <li className="bg-green-800 p-2 rounded">Portfolio Enhancement: Showcase your creativity and talent by adding your logo design to your portfolio. It&apos;s a great opportunity to impress potential clients and employers.</li>
+            <li className="bg-gray-800 p-2 rounded">Contribution: Leave a lasting impact by contributing to the visual identity of our platform, shaping its brand image for years to come.</li>
           </ol>
           <h2 className="text-2xl font-bold mt-6 mb-2">Design Guidelines:</h2>
           <ul className="list-disc list-inside space-y-2">
-            <li className="bg-gray-800 p-2 rounded"><strong>Innovation 🚀:</strong> Encourage designs that symbolize creativity, originality, and the pursuit of new ideas. Let the logo represent the platform commitment to innovation and forward-thinking.</li>
-            <li className="bg-gray-500 p-2 rounded"><strong>Collaboration 🤝:</strong> Highlight the importance of teamwork and cooperation. Incorporate elements that convey people working together towards a common goal, fostering a sense of collaboration within the community.</li>
-            <li className="bg-gray-800 p-2 rounded"><strong>Connection 🌐:</strong> Emphasize networking and connectivity. Use visual cues like network nodes, interlocking shapes, or symbolic gestures to represent the platform role in facilitating meaningful connections among its users.</li>
-            <li className="bg-gray-500 p-2 rounded"><strong>Learning 📚:</strong> Reflect the platform dedication to education and knowledge sharing. Integrate symbols of learning, growth, and exploration to inspire curiosity and intellectual curiosity.</li>
-            <li className="bg-gray-800 p-2 rounded"><strong>Community 🏘️:</strong> Capture the spirit of community and inclusivity. Choose imagery that represents diversity, unity, and mutual support, fostering a welcoming environment for all users.</li>
-            <li className="bg-gray-500 p-2 rounded"><strong>Empowerment 💪:</strong> Communicate the platform mission to empower individuals to make a difference. Let the logo inspire confidence, resilience, and the belief that every voice has value and impact.</li>
-            <li className="bg-gray-800 p-2 rounded"><strong>Adaptability 🔄:</strong> Design for versatility across different contexts and platforms. Ensure the logo remains impactful and recognizable whether displayed online, in print, or on merchandise.</li>
-            <li className="bg-gray-500 p-2 rounded"><strong>Futurism 🌟:</strong> Infuse elements of modernity and progress. Utilize sleek lines, geometric shapes, or futuristic motifs to convey a forward-looking perspective and embrace the possibilities of tomorrow.</li>
-            <li className="bg-gray-800 p-2 rounded"><strong>Simplicity ✨:</strong> Strive for simplicity and clarity in the design. Opt for a minimalist approach to ensure the logo is easily memorable, scalable, and timeless.</li>
-            <li className="bg-gray-500 p-2 rounded"><strong>Brand Identity 💼:</strong> Align the logo with the platform overall brand identity and values. Ensure it resonates with the target audience while conveying a unique and compelling message about the platform purpose and vision.</li>
+            <li className="bg-green-800 p-2 rounded"><strong>Innovation 🚀:</strong> Encourage designs that symbolize creativity, originality, and the pursuit of new ideas. Let the logo represent the platform commitment to innovation and forward-thinking.</li>
+            <li className="bg-gray-800 p-2 rounded"><strong>Collaboration 🤝:</strong> Highlight the importance of teamwork and cooperation. Incorporate elements that convey people working together towards a common goal, fostering a sense of collaboration within the community.</li>
+            <li className="bg-green-800 p-2 rounded"><strong>Connection 🌐:</strong> Emphasize networking and connectivity. Use visual cues like network nodes, interlocking shapes, or symbolic gestures to represent the platform role in facilitating meaningful connections among its users.</li>
+            <li className="bg-gray-800 p-2 rounded"><strong>Learning 📚:</strong> Reflect the platform dedication to education and knowledge sharing. Integrate symbols of learning, growth, and exploration to inspire curiosity and intellectual curiosity.</li>
+            <li className="bg-green-800 p-2 rounded"><strong>Community 🏘️:</strong> Capture the spirit of community and inclusivity. Choose imagery that represents diversity, unity, and mutual support, fostering a welcoming environment for all users.</li>
+            <li className="bg-gray-800 p-2 rounded"><strong>Empowerment 💪:</strong> Communicate the platform mission to empower individuals to make a difference. Let the logo inspire confidence, resilience, and the belief that every voice has value and impact.</li>
+            <li className="bg-green-800 p-2 rounded"><strong>Adaptability 🔄:</strong> Design for versatility across different contexts and platforms. Ensure the logo remains impactful and recognizable whether displayed online, in print, or on merchandise.</li>
+            <li className="bg-gray-800 p-2 rounded"><strong>Futurism 🌟:</strong> Infuse elements of modernity and progress. Utilize sleek lines, geometric shapes, or futuristic motifs to convey a forward-looking perspective and embrace the possibilities of tomorrow.</li>
+            <li className="bg-green-800 p-2 rounded"><strong>Simplicity ✨:</strong> Strive for simplicity and clarity in the design. Opt for a minimalist approach to ensure the logo is easily memorable, scalable, and timeless.</li>
+            <li className="bg-gray-800 p-2 rounded"><strong>Brand Identity 💼:</strong> Align the logo with the platform overall brand identity and values. Ensure it resonates with the target audience while conveying a unique and compelling message about the platform purpose and vision.</li>
           </ul>
           <h2 className="text-2xl font-bold mt-6 mb-2">Terms and Conditions:</h2>
           <ol className="list-disc list-inside space-y-2">
-            <li className="bg-gray-800 p-2 rounded">By submitting your logo design, you agree to grant us the rights to use, modify, and reproduce the design for promotional and branding purposes.</li>
-            <li className="bg-gray-500 p-2 rounded">Designs must not contain any copyrighted material or infringe upon the intellectual property rights of others.</li>
-            <li className="bg-gray-800 p-2 rounded">The decision of the judging panel is final. Winners will be selected based on creativity, relevance to the theme, and overall aesthetic appeal.</li>
+            <li className="bg-green-800 p-2 rounded">By submitting your logo design, you agree to grant us the rights to use, modify, and reproduce the design for promotional and branding purposes.</li>
+            <li className="bg-gray-800 p-2 rounded">Designs must not contain any copyrighted material or infringe upon the intellectual property rights of others.</li>
+            <li className="bg-green-800 p-2 rounded">The decision of the judging panel is final. Winners will be selected based on creativity, relevance to the theme, and overall aesthetic appeal.</li>
           </ol>
           <p className="mt-6">Get ready to unleash your creativity and be part of something extraordinary! Submit your logo design today and let your talent shine! 🚀✨</p>
         </div>
 
-      </div>
       </div>
   );
 };
