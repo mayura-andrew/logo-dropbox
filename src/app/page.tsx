db6416180a1e4f5abf7a3d3d4099d11f633bc69b
mayura-andrew <mayuraalahakoon@gmail.com>
2024-03-19 10:40:57 +0530

    fixed typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
             className="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded " 
             onClick={() => setShowDetails(!showDetails)}
           >
-            {showDetails ? 'Hide Details ⬆️ ' : 'About Competition ⬅️'}
+            {showDetails ? 'Hide Details ⬆️ ' : 'About this Competition ⬅️'}
           </button>
         </div>   
       </div>
